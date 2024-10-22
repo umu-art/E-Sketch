a04a8ab11fa43084106b2aac9c7b8ef87b7c278d
Казенин Владимир <kazeninvladimir@gmail.com>
2024-10-22 23:54:45 +0300

    fix pg connection

--- est-back/main.cpp
+++ est-back/main.cpp
@@ -2,7 +2,24 @@
 #include "PingController.hpp"
 
 int main() {
-    drogon::app().loadConfigFile("../config.yaml");
+    // Load config
+    drogon::app().loadConfigFile("../config.json");
+
+    // Register db client
+    auto dbConfig = drogon::orm::PostgresConfig{
+            .host = "postgres.est-dbs.svc.cluster.local",
+            .port = 5432,
+            .databaseName = "est-data",
+            .username = getenv("POSTGRES_USERNAME"),
+            .password = getenv("POSTGRES_PASSWORD"),
+            .connectionNumber = 10,
+            .name = "est-data",
+            .timeout = 60,
+            .autoBatch = true
+    };
+    drogon::app().addDbClient(dbConfig);
+
+    // Start
     drogon::app().run();
     return 0;
 }
