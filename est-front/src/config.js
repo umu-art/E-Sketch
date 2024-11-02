903e79142171b5824d95b67014750ba623da0f42
pavel <pavel@desktop-33fo31s>
2024-11-02 13:59:26 +0300

    add smth

--- est-front/src/config.js
+++ est-front/src/config.js
@@ -1,4 +1,16 @@
-const back_url = 'https://e-sketch.ru'
+let back_url = 'https://e-sketch.ru'
+
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+if (isLocalhost) {
+    back_url = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port
+}
 
 export const Config = {
     back_url: back_url,
