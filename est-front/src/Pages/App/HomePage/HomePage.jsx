c52662ac96873fc798b2427ec59c6649c639f07b
pavel <pavel@DESKTOP-33FO31S>
2024-11-04 19:46:05 +0300

    add MyBoardsPage and CreateBoardForm

---
+++ est-front/src/Pages/App/HomePage/HomePage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { Flex } from 'antd';
+
+import Menu from './Menu/Menu.jsx';
+import MyBoardsPage from './Pages/MyBoardsPage/MyBoardsPage.jsx';
+
+
+const HomePage = () => {
+    return (
+        <Flex className="mh100vh" style={{padding: "50px 100px"}}>
+            <Flex className='h100p'>
+               <Menu />
+            </Flex>
+            <Routes>
+                <Route path='my' element={<MyBoardsPage />}/>
+                <Route path='*' element={<Navigate to="my" />}/> 
+                <Route path='' element={<Navigate to="my" />}/> 
+            </Routes>
+        </Flex>
+    );
+};
+
+export default HomePage;
