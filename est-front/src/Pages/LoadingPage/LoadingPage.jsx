ade1f4737136eb642c51c6c89be55f1f51094d9b
pavel <pavel@DESKTOP-33FO31S>
2024-11-15 22:24:08 +0300

    add BoardPage and UpdateBoardForm

---
+++ est-front/src/Pages/LoadingPage/LoadingPage.jsx
@@ -0,0 +1,16 @@
+import { Flex } from 'antd';
+import React from 'react';
+
+import { LoadingOutlined } from '@ant-design/icons';
+
+const LoadingPage = () => {
+    return (
+        <Flex className='w100vw h100vh' style={{fontSize: 50}} justify='center' align='center'>
+            <Flex style={{width: 100, height: 100}} justify='center' align='center'>
+                <LoadingOutlined />
+            </Flex>
+        </Flex>
+    );
+};
+
+export default LoadingPage;
