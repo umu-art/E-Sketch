d061ae899ee6fe5d5475cd95282c8b2b4966cb6e
Казенин Владимир <kazeninvladimir@gmail.com>
2025-01-18 01:57:35 +0300

    est-test

--- est-test/src/main.js
+++ est-test/src/main.js
@@ -1,13 +1,13 @@
 import { BoardApi, UserApi } from 'est_proxy_api';
 import { FigureType, Line, Point } from 'figures/dist/index.js';
 import { encode } from 'coder/dist/index.js';
+import { WebSocket } from 'ws';
 
 let userApi = new UserApi();
 let boardApi = new BoardApi();
 
-const BOARD_COUNT = 16;
-const USER_COUNT = 16;
-const TEST_TIMEOUT = 5 * 60 * 1000;
+const MAX_BOARD_COUNT = 4;
+const MAX_USER_COUNT = 4;
 
 async function main() {
   let authCookie = await userApi.loginWithHttpInfo({
@@ -25,46 +25,55 @@ async function main() {
     Cookie: authCookie,
   };
 
-  return Promise.all(Array(BOARD_COUNT).fill(null).map((_, i) => {
-    return boardApi.create({
-      createRequest: {
-        name: 'Test board at ' + i + ' at ' + new Date().toISOString(),
-        description: 'This is a test board',
-        linkSharedMode: 'none_by_link',
-      },
-    }).then(board => imitateBoardFrFrFr(board, USER_COUNT, authCookie));
-  }));
+  for (let boardCount = 1; boardCount < MAX_BOARD_COUNT; boardCount++) {
+    for (let userCount = 1; userCount < MAX_USER_COUNT; userCount++) {
+      console.log('Starting with ' + boardCount + ' boards and ' + userCount + ' users');
+      await Promise.all(Array(boardCount).fill(null).map((_, i) => {
+        return boardApi.create({
+          createRequest: {
+            name: 'Test board at ' + i + ' at ' + new Date().toISOString(),
+            description: 'This is a test board',
+            linkSharedMode: 'none_by_link',
+          },
+        }).then(board => imitateBoardFrFrFr(board, userCount, authCookie));
+      }));
+      console.log('Finished with ' + boardCount + ' boards and ' + userCount + ' users');
+    }
+  }
 }
 
 async function imitateBoardFrFrFr(board, usersCount, authCookie) {
-  return Promise.all(Array(usersCount).fill(null)
+  const startTime = new Date().getTime();
+
+  await Promise.all(Array(usersCount).fill(null)
     .map((_) => imitateUserFrFrFr(board, authCookie)));
+
+  const endTime = new Date().getTime();
+  console.log('Board ' + board.id + ' with ' + usersCount + ' users imitated in ' + (endTime - startTime) + ' ms');
 }
 
 async function imitateUserFrFrFr(board, authCookie) {
-  let startTime = new Date().getTime();
   let markerWebSocket = new WebSocket('wss://e-sketch.ru/proxy/marker/ws?boardId=' + board.id, {
     headers: {
       Cookie: authCookie,
     },
   });
 
-  while (new Date().getTime() - startTime < TEST_TIMEOUT) {
-    let figureWebSocket = new WebSocket('wss://e-sketch.ru/proxy/figure/ws?boardId=' + board.id, {
-      headers: {
-        Cookie: authCookie,
-      },
-    });
+  let figureWebSocket = new WebSocket('wss://e-sketch.ru/proxy/figure/ws?boardId=' + board.id, {
+    headers: {
+      Cookie: authCookie,
+    },
+  });
 
-    await new Promise((resolve, reject) => {
-      figureWebSocket.onopen = resolve;
-      figureWebSocket.onerror = reject;
-    });
+  await new Promise((resolve, _) => {
+    figureWebSocket.onopen = resolve;
+  });
 
+  for (let i = 0; i < 30; i++) {
     await createFigure(figureWebSocket);
-    figureWebSocket.close();
   }
 
+  figureWebSocket.close();
   markerWebSocket.close();
 }
 
@@ -82,11 +91,12 @@ async function createFigure(figureWebSocket) {
       figureWebSocket.send(String.fromCharCode(0));
     });
 
-    let figure = new Line(FigureType.LINE, figureId, ['red', '3'], []);
+    let figure = new Line(FigureType.LINE, figureId, ['blue', '3'], []);
+
     figureWebSocket.send(String.fromCharCode(1) + encode(figure));
     await sleep(1000 / 60);
 
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 120; i++) {
       const oldFigureEncoded = encode(figure);
       figure.points.push(new Point(Math.random() * 1000, Math.random() * 1000));
       const newFigureEncoded = encode(figure);
@@ -95,9 +105,7 @@ async function createFigure(figureWebSocket) {
       figureWebSocket.send(String.fromCharCode(3) + String.fromCharCode(figure.type) + figure.id + newFigurePart);
       await sleep(1000 / 60);
     }
-
-    console.log('Created figure with ID:', figureId);
-  }catch (e) {
+  } catch (e) {
     console.log(e);
   }
 }
