842845fcb5db88639eb764ed052b598c5cc166a6
pavel <pavel@DESKTOP-33FO31S>
2025-01-22 19:25:36 +0300

    fix Marker style

--- est-front/src/Pages/App/BoardPage/Board/Markers.js
+++ est-front/src/Pages/App/BoardPage/Board/Markers.js
@@ -8,6 +8,38 @@ const apiInstance = new UserApi();
 
 const timeoutDuration = 10000;
 
+const colorPalette = [
+  "#FF5733", // Красный
+  "#33FF57", // Зеленый
+  "#3357FF", // Синий
+  "#F3FF33", // Желтый
+  "#FF33A1", // Розовый
+  "#33FFF7", // Бирюзовый
+  "#B833FF", // Фиолетовый
+  "#FFC300", // Золотой
+  "#DAF7A6", // Лаймовый
+  "#FF3377"  // Ярко-розовый
+];
+
+const colorCache = {};
+
+function generateColorFromUsername(username) {
+  if (colorCache[username]) {
+    return colorCache[username];
+  }
+
+  let hash = 0;
+  for (let i = 0; i < username.length; i++) {
+      hash = username.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const colorIndex = Math.abs(hash) % colorPalette.length;
+
+  colorCache[username] = colorPalette[colorIndex];
+  
+  return colorPalette[colorIndex];
+}
+
 export function registerMarkersListener(board) {
   let markersMap = new Map();
   let timersMap = new Map();
@@ -44,10 +76,16 @@ export function registerMarkersListener(board) {
     const windowHeight = window.innerHeight;
 
     const marker = document.createElement('p');
+    
     marker.textContent = username;
+    
     marker.style.pointerEvents = 'none';
     marker.style.position = 'absolute';
     marker.style.visibility = 'hidden';
+
+    marker.style.padding = '5px 10px';
+    marker.style.borderRadius = '10px 10px 10px 0px';
+    marker.style.boxShadow = '2px 2px 5px rgba(0, 0, 0, 0.3)';
 
     document.body.appendChild(marker);
 
@@ -59,12 +97,12 @@ export function registerMarkersListener(board) {
     const x = (point.x + drawing.offsetX) * drawing.scale;
     const y = (point.y + drawing.offsetY) * drawing.scale;
 
-    const safeX = Math.min(Math.max(x + 10, 10), windowWidth - (markerWidth + 10));
-    const safeY = Math.min(Math.max(y - 10, 10), windowHeight - (markerHeight + 10));
+    const safeX = Math.min(Math.max(x, 0), windowWidth - markerWidth);
+    const safeY = Math.min(Math.max(y - markerHeight, 0), windowHeight - markerHeight - 1);
 
     marker.style.left = `${safeX}px`;
     marker.style.top = `${safeY}px`;
-    marker.style.backgroundColor = 'yellow';
+    marker.style.backgroundColor = generateColorFromUsername(username);
     marker.style.userSelect = 'none';
     marker.style.visibility = 'visible';
     marker.style.zIndex = 10;
