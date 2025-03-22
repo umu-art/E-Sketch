d815dda51dc0c5822417a7ae6d7364d16700a9c0
Казенин Владимир <kazeninvladimir@gmail.com>
2025-03-22 21:51:41 +0300

    add dkim

--- est-back/main.cpp
+++ est-back/main.cpp
@@ -8,11 +8,11 @@ int main() {
     // Register db client
     auto dbConfig = drogon::orm::PostgresConfig{.host = "192.168.31.10",
                                                 .port = 5432,
-                                                .databaseName = "e-sketch",
+                                                .databaseName = getenv("POSTGRES_USERNAME"),
                                                 .username = getenv("POSTGRES_USERNAME"),
                                                 .password = getenv("POSTGRES_PASSWORD"),
                                                 .connectionNumber = 10,
-                                                .name = getenv("POSTGRES_USERNAME"),
+                                                .name = "e-sketch",
                                                 .timeout = 60,
                                                 .autoBatch = true};
     drogon::app().addDbClient(dbConfig);
